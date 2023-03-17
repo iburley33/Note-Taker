3a160c747372046442ba40dda3a29baa6b365055
iburley33 <iburley33@gmail.com>
2023-03-16 21:56:01 -0400

    almost done

---
+++ Develop/Routes/apiRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/notes', (req, res) => {
+
+});
+router.post('/notes', (req, res) => {
+    
+});
+router.delete('/notes/:id', (req, res) => {
+    
+});
+
+module.exports = router;
---
+++ Develop/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const api = require('./Routes/apiRoutes');
+const html = require('./Routes/htmlRoutes');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', api);
+app.use('/', html);
+
+// set up routes for /notes as referred to in the provided code. 
+
+
+app.listen(PORT, () =>
+console.log(`Example app listening at http://localhost:${PORT}`)
+);
+
+
+
+
+// localhost:3001/api/note/4
