c351386199411c54f6101f0894830f4b7b0ae285
iburley33 <iburley33@gmail.com>
2023-03-23 21:16:11 -0400

    final

--- Develop/server.js
+++ Develop/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('Develop/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', api);
 app.use('/', html);
 
